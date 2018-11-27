7fd19ed809ae6e409328ab14b473e7191983d9f2
ryanazrian <azrian.ryan@gmail.com>
2018-11-27 20:18:42 +0700

    haha

--- app/Http/Controllers/ArraySpanTaskController.php
+++ app/Http/Controllers/ArraySpanTaskController.php
@@ -8,6 +8,6 @@ class ArraySpanTaskController extends Controller
 {
     //
     public function index(){
-        return view('ArraySpanTask');
+        return view('pages/tester/ArraySpanTask');
     }
 }
